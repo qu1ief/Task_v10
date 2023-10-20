1ac188c48a37cb263885884385cb44cabf6b717b
Sabir164 <sabirkg@code.edu.az>
2023-10-20 09:26:39 -0700

    taskv10

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿#region task1
+//int counter = 0;
+//Console.WriteLine("A ededini daxil edin..");
+//int numA = int.Parse(Console.ReadLine());
+//Console.WriteLine("B ededini daxil edin..");
+//int numB = int.Parse(Console.ReadLine());
+//int result = numA + numB;
+//string resultv10 = result.ToString();
+//foreach (var num in resultv10)
+//{
+//    counter++;
+//}
+//if (counter == 2)
+//{
+//    Console.WriteLine("Cem 2 reqemlidir.");
+//}
+//else if (counter == 3)
+//{
+//    Console.WriteLine("Cem 3 reqemlidir.");
+//}
+//else
+//{
+//    Console.WriteLine("Nezere alinmayib..");
+//}
+#endregion
+#region task2
+//Console.WriteLine("Skafin olculerini daxil edin");
+//Console.Write("eni:");
+//int eni = int.Parse(Console.ReadLine());
+//Console.Write("hundurluyu:");
+//int hundurluk = int.Parse(Console.ReadLine());
+//Console.Write("uzunluq:");
+//int uzunluq = int.Parse(Console.ReadLine());
+
+//int e = 100;
+//int h = 200;
+
+//if ((eni <= e && hundurluk <= h) || (eni <= e && uzunluq <= h) || (uzunluq <= e && hundurluk <= h) || (uzunluq <= e && eni <= h)
+//    || (hundurluk <= e && uzunluq <= h) || (hundurluk <= e && eni <= h)
+//    )
+//{
+//    Console.WriteLine("skaf qapidan kecir..");
+//}
+
+//else
+//{
+//    Console.WriteLine("skaf qapidan kecmir");
+
+//}
+#endregion
